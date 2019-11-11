fe547384afdd8e19f74161867143adee06a5dfb6
James Dube <jamesdylandube@gmail.com>
2019-11-11 13:28:03 +0200

    Fix grammar

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,7 +4,7 @@
             <div class="text-brand-accent font-foo mb-4">Hi, my name is</div>
             <div class="text-7xl text-white font-black font-bar">Jame Dube</div>
             <div class="text-7xl font-black font-bar">I build things for the web.</div>
-            <div class="xl:w-1/2 leading-relaxed my-8">I'm a software engineer based with Ops experience, specializing in building (and occasionally designing) exceptional, high-quality websites and applications.
+            <div class="xl:w-1/2 leading-relaxed my-8">I'm a software engineer with Ops experience, specializing in building (and occasionally designing) exceptional, high-quality websites and applications.
             </div>
             <button class="border-brand-accent border rounded mt-16 px-8 py-4 text-brand-accent flex self-center ">
                 Get in touch
