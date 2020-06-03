ea45915391381ce82b5cb75b3bedde2409aeef3a
James Dube <jamesdylandube@gmail.com>
2020-06-03 21:29:10 +0200

    first iteration demo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App3.vue'
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-153997790-1',
+  checkDuplicatedScript: true
+})
 
 Vue.config.productionTip = false
 
